b47ebd400057be7b21acb492ec5bb876af7cb927
BobObi <123968601+BoboObi@users.noreply.github.com>
2023-05-20 19:34:28 -0400

    Basic React Node Express mysql Project - created Folder with client and backend folders - initialised node using (npm init -y: with -y skippig questions and using defaults ) - Ran (npm i express mysql nodemon) to install three packages (express, mysql, and nodemon) in the Node.js project - Iported express and ued the express object to build the web server on port 8800

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+// Define routes and middleware
+// ...
+
+// Start the server
+app.listen(8800, () => {
+  console.log("App is connected to backend ---*** @PORT :8800 ***---");
+});
