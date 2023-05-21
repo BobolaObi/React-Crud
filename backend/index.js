cb67c89ab8f73a67881733731c3f1b6ff311a3ce
BobObi <123968601+BoboObi@users.noreply.github.com>
2023-05-20 20:03:33 -0400

    feat: Add route handler for GET /books endpoint
    This commit introduces a new route handler for handling GET requests to the /books URL endpoint. When a client makes a GET request to /books, the server executes a SQL query to fetch all rows from the books table using the  connection. The query result is then sent as a JSON response to the client.
    
    This feature enables retrieval of book data from the backend API, making it accessible for client applications to consume.

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,14 @@ app.get("/", (req, res) =>{
     res.json("Hello this is the backend!")
 })
 
+app.get("/books", (req, res) =>{
+    const q = "SELECT * FROM books";
+    database.query(q, (err, data)=> {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
 // Start the server
 app.listen(8800, () => {
   console.log("App is connected to backend ---*** @PORT :8800 ***---");
