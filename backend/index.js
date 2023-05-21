f0c2d49dbe51b5894fa4a73a5579bb900cd67095
BobObi <123968601+BoboObi@users.noreply.github.com>
2023-05-20 20:56:24 -0400

    feat: Creating a new connection to the database for each request to fix: - (PROTOCOL_ENQUEUE_AFTER_FATAL_ERROR)

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,21 @@ import express from "express";
 import mysql from "mysql";
 
 const app = express();
-const database = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"test"
-})
 
+function queryDatabase(query, values, callback) {
+    const database = mysql.createConnection({
+        host:"localhost",
+        user:"root",
+        password:"",
+        database:"test"
+    });
+
+    database.query(query, values, (err, data) => {
+        // Always close the database connection when we're done
+        database.end();
+        callback(err, data);
+    });
+}
 // Define routes and middleware
 app.get("/", (req, res) =>{
     res.json("Hello this is the backend!")
@@ -20,6 +28,18 @@ app.get("/books", (req, res) =>{
         if(err) return res.json(err)
         return res.json(data)
     })
+})
+
+app.post("/books", (req, res)=>{
+    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
+    const values = ["Book Title", "Book Description", "book_cover.jpg"];
+
+    database.query(q, [values], (err, data)=> {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+
+
 })
 
 // Start the server
