bb50e278dcea7c979994774b57af18de97119805
BobObi <123968601+BoboObi@users.noreply.github.com>
2023-05-20 22:27:43 -0400

    Fix: Issue with posr request: - Called wrong method on espress JSON - Commented code

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,24 @@
 import express from "express";
 import mysql from "mysql";
 
+// Create an Express app
 const app = express();
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Function to query the database
 function queryDatabase(query, values, callback) {
+    // Create a connection to the MySQL database
     const database = mysql.createConnection({
-        host:"127.0.0.1",
-        user:"root",
-        password:"",
-        database:"test",
+        host: "127.0.0.1",
+        user: "root",
+        password: "",
+        database: "test",
         port: 3306 
     });
 
+    // Execute the query with provided values
     database.query(query, values, (err, data) => {
         // Always close the database connection when we're done
         database.end();
@@ -19,34 +26,35 @@ function queryDatabase(query, values, callback) {
     });
 }
 
+// Root endpoint
 app.get("/", (req, res) => {
-    res.json("Hello this is the backend!")
-})
+    res.json("Hello this is the backend!");
+});
 
+// Endpoint to fetch all books
 app.get("/books", (req, res) => {
     const q = "SELECT * FROM books";
     queryDatabase(q, [], (err, data) => {
-        if(err) return res.json(err);
+        if (err) return res.json(err);
         return res.json(data);
     });
 });
 
-app.use(express.json());
-
+// Endpoint to create a new book
 app.post("/books", (req, res) => {
-    const q = "INSERT INTO books (`title`, `description`, `cover`) VALUES (?)"
+    const q = "INSERT INTO books (`title`, `description`, `cover`) VALUES (?)";
     const values = [
       req.body.title,
       req.body.description,
       req.body.cover,
-
     ];
     queryDatabase(q, [values], (err, data) => {
-        if(err) return res.json(err);
+        if (err) return res.json(err);
         return res.json(data);
     });
 });
 
+// Start the server
 app.listen(8800, () => {
     console.log("App is connected to backend ---*** @PORT :8800 ***---");
 });
