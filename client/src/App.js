6f666fb45bb4ee8e60c88360de85497d993da42a
BobObi <123968601+BoboObi@users.noreply.github.com>
2023-05-21 23:36:39 -0400

    Installed Axios and React Router Dom
    + Added React Router Dom for smooth page routing
    + Integrated Axios for handling HTTP requests and data retrieval
    
    --This commit installs Axios and React Router Dom dependencies. React Router Dom enables seamless page navigation, while Axios provides a convenient way to handle HTTP requests and retrieve data from APIs or server endpoints. These libraries enhance the project's functionality, improving routing and data handling capabilities.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Books from './pages/Books';
+import Add from './pages/Add';
+import Update from './pages/Update';
 
 function App() {
   return (
     <div className="App">
-    <h1>
-      Hello React Nerds
-    </h1>
-
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Books/>}/>
+        <Route path="/add" element={<Add/>}/>
+        <Route path="/update" element={<Update/>}/>
+      </Routes>
+      </BrowserRouter>
+ 
     </div>
   );
 }
